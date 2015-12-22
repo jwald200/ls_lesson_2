604faa508aba388ea59b0884c2f8a28f4ddf7d8c
Joe <joe@example.com>
2015-12-22 13:34:49 -0500

    adds RPS third bonus feature

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -9,42 +9,79 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def display_results(player, computer)
-  if player == computer
-    prompt("It's a tie!")
+def winner_name(player, computer)
+  players = [player, computer]
+  winner = nil
+  WINNING_SCENARIOS.each do |choice, defeated|
+    next if !players.include?(choice) || (players & defeated).empty?
+    winner = (choice == player ? 'you' : 'computer')
+  end
+  winner
+end
+
+def game_over?(player_score, computer_score)
+  rounds_to_win = 5
+  (player_score == rounds_to_win) || (computer_score == rounds_to_win)
+end
+
+def scores(player_score, computer_score)
+  "Player's score: #{player_score} Computer's score: #{computer_score}"
+end
+
+def display_results(winner, player_score, computer_score)
+  prompt(scores(player_score, computer_score))
+  if game_over?(player_score, computer_score)
+    prompt("Game over! #{winner} won 5 rounds")
+  elsif winner
+    prompt("#{winner} won!")
   else
-    WINNING_SCENARIOS.each do |winner, loser|
-      players = [player, computer]
-      next if !players.include?(winner) || (players & loser).empty?
-      prompt("#{(winner == player ? 'You' : 'Computer')} won!")
-    end
+    prompt("It's a tie!")
   end
 end
 
-loop do
-  choice = ''
-
+def player_turn
   loop do
     prompt("Choose one: #{WINNING_SCENARIOS.keys.join(', ')}")
     choice = gets.chomp
+
     if choice == 's'
       prompt("Did you mean scissors or spock? use sp/sc")
       next
     end
-    choice = WINNING_SCENARIOS.keys.detect { |winner| winner.start_with?(choice)}
+    choice = WINNING_SCENARIOS.keys.detect { |winning_choice| winning_choice.start_with?(choice) }
 
-    if WINNING_SCENARIOS.include?(choice)
-      break
+    if WINNING_SCENARIOS.keys.include?(choice)
+      return choice
     else
       prompt("That's not a valid choice.")
     end
   end
+end
 
-  computer_choice = WINNING_SCENARIOS.keys.sample
+def computer_turn
+  WINNING_SCENARIOS.keys.sample
+end
 
-  puts "You chose: #{choice}; Computer chose: #{computer_choice}"
+loop do
+  system 'clear'
+  player_score = 0
+  computer_score = 0
 
-  display_results(choice, computer_choice)
+  until game_over?(player_score, computer_score)
+    player_choice = player_turn
+    computer_choice = computer_turn
+    winner = winner_name(player_choice, computer_choice)
+
+    if winner == 'you'
+      player_score += 1
+    elsif winner == 'computer'
+      computer_score += 1
+    end
+
+    system 'clear'
+    puts "You chose: #{player_choice}; Computer chose: #{computer_choice}"
+    display_results(winner, player_score, computer_score)
+  end
 
   prompt("would you like to play again? (Y/N)")
   break if gets.chomp.downcase.start_with?('n')
